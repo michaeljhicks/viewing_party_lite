e241cddf30aa02015e2550d751ab2a9b0ac64b25
Michael Hicks <hicks.michael04@gmail.com>
2022-05-25 00:45:28 -0600

    feat: build and establish model relationships

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,3 +1,9 @@
 class ViewingParty < ApplicationRecord
-  belongs_to :user
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :duration, presence: true
+  validates :movie_title, presence: true
+
+  has_many :party_users
+  has_many :users, through: :party_users
 end
