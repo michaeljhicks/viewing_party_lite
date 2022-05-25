f34171f9a05a6a60ea5b24a55c3d08046afda821
Michael Hicks <hicks.michael04@gmail.com>
2022-05-25 02:45:14 -0600

    refactor: is_host? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :viewing_parties, through: :party_users
 
   def is_host?(party_id)
-    PartyUser.find_by(user_id: self.id, viewing_party_id: party_id).host
+    PartyUser.find_by(user_id: id, viewing_party_id: party_id).host
   end
 end
