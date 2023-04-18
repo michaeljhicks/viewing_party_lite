8d70216c730f5fa1e058a5e9ec42d7114eb82a7e
Michael Hicks <hicks.michael04@gmail.com>
2023-04-18 10:04:12 -0600

    test: discover and movie specs

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,44 +1,70 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe 'The movie details page' do
+RSpec.describe 'movie show page' do
   before(:each) do
-    @user = User.create!(name: 'Jackie', email: 'jackie.brown@gmail.com', password: 'test', password_confirmation: 'test')
-    @movie_id = 550
+    @user1 = User.create!(name: 'Mike', email: 'mike@example.com', password: 'test')
+    visit login_form_path
+    fill_in 'email', with: 'mike@example.com'
+    fill_in 'password', with: 'test'
+    click_button 'Submit'
+    @movie1 = Movie.new(title: 'Scarface', id: 111, runtime: 170, vote_average: 8.2, summary: 'After getting a green card in exchange for assassinating a Cuban government official, Tony Montana stakes a claim on the drug trade in Miami. Viciously murdering anyone who stands in his way, Tony eventually becomes the biggest drug lord in the state, controlling nearly all the cocaine that comes through Miami. But increased pressure from the police, wars with Colombian drug cartels and his own drug-fueled paranoia serve to fuel the flames of his eventual downfall.', poster: '/32pLDObtIt2MJcdPG9mQKuybImL.jpg', genres: [
+                          {
+                            "id": 28,
+                            "name": 'Action'
+                          },
+                          {
+                            "id": 80,
+                            "name": 'Crime'
+                          },
+                          {
+                            "id": 18,
+                            "name": 'Drama'
+                          },
+                          {
+                            "id": 53,
+                            "name": 'Thriller'
+                          }
+                        ])
 
-    visit '/'
-    click_on 'Login'
-    fill_in :email, with: @user.email
-    fill_in :password, with: @user.password
-    click_on 'Login'
-    visit "/dashboard/movies/#{@movie_id}"
+    visit movie_path(@movie1.id)
   end
-
-  it 'displays a link to create a new viewing party' do
-    click_on 'Create Viewing Party'
-
-    expect(current_path).to eq("/dashboard/movies/#{@movie_id}/new")
+  
+  it 'has a button to create a viewing party', :vcr do
+    click_button('Create Viewing Party for Scarface')
   end
-
-  it 'displays movie details for movie' do
-    text = "A ticking-time-bomb insomniac and a slippery"
-    expect(page).to have_content('Fight Club')
-    expect(page).to have_content('Vote Average 8.4')
-    expect(page).to have_content('Runtime: 2 hours 19 minutes')
-    expect(page).to have_content("Description: #{text}")
-    expect(page).to have_content('Genre(s): Drama')
+  
+  it 'displays the movie title', :vcr do
+    expect(page).to have_content('Scarface')
   end
-
-  it 'displays cast information' do
-    expect(page).to have_content('The Narrator: Edward Norton')
-    expect(page).to have_content('Tyler Durden: Brad Pitt')
-    expect(page).to have_content('Marla Singer: Helena Bonham Carter')
+  
+  it "display's the movie's vote average", :vcr do
+    expect(page).to have_content('Vote Average: 8.2')
   end
-
-  it 'displays review information' do
-    expect(page).to have_content('7 reviews')
-    expect(page).to have_content('Author: Goddard')
-    expect(page).to have_content('Review: Pretty awesome movie.')
-    expect(page).to have_content('Author: Brett Pascoe')
-    expect(page).to have_content('Review: In my top 5 of all time favourite movies')
+  
+  it "displays the movie's runtime", :vcr do
+    expect(page).to have_content('Runtime: 170mins')
+  end
+  
+  it "displays the movie's summary", :vcr do
+    expect(page).to have_content('After getting a green card in exchange for assassinating a Cuban government official, Tony Montana stakes a claim on the drug trade in Miami. Viciously murdering anyone who stands in his way, Tony eventually becomes the biggest drug lord in the state, controlling nearly all the cocaine that comes through Miami. But increased pressure from the police, wars with Colombian drug cartels and his own drug-fueled paranoia serve to fuel the flames of his eventual downfall.')
+  end
+  
+  it 'has a button to return to Discover page', :vcr do
+    expect(page).to have_button('Discover Page')
+    
+    click_button('Discover Page')
+    
+    expect(current_path).to eq(discover_index_path)
+  end
+  
+  it 'user must be logged in to create a viewing party', :vcr do 
+    click_button 'Logout'
+    
+    visit movie_path(@movie1.id)
+    click_button('Create Viewing Party for Scarface')
+    expect(current_path).to eq(movie_path(@movie1.id))
+    expect(page).to have_content("You must be logged in or registered to create a viewing party")
   end
 end
